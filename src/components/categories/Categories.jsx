67eaf6cbb05cf48ec98a841808260133ad2153aa
Cesare-Tomasello <cesarinho@live.it>
2023-01-25 00:57:23 +0100

    esercitazione 24-01-23

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -5,22 +5,33 @@ import { GET } from "../libs/HTTPS";
 function Categories() {
   const listObject = {
     categories: [],
-    loading: false,
+    loading: true,
   };
 
   const [list, setList] = useState(listObject);
 
   useEffect(() => {
-    GET("categories").then((data) => setList({ ...list, categories: data }));
+    GET("categories").then((data) =>
+      setList({ loading: false, categories: data })
+    );
   }, []);
 
   return (
     <div className={categories.main}>
-      <h1>CATEGORIES</h1>
-      {/* DA COMPLETARE */}
-      {/* {list.categories.map((item) => (
-        <div>{item}</div>
-      ))} */}
+      <h1 className={categories.title}>CATEGORIES</h1>
+      {listObject.loading ? "loading..." : "caricato"}
+      <div className={categories.container}>
+        {list.categories.map((item) => (
+          <div className={categories.card} key={item.id}>
+            <h3 className={categories.name}>{item.name}</h3>
+            <img
+              className={categories.image}
+              src={item.image}
+              alt={item.name}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
